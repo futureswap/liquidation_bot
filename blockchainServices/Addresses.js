f5dc2007c835d55bb257f4d7e96db7440ce048ee
JesseAbram <jesseabramowitz@hotmail.com>
2020-03-04 10:16:30 -0500

    update chainlink address

--- blockchainServices/Addresses.js
+++ blockchainServices/Addresses.js
@@ -1,5 +1,5 @@
  const exchangeAddresses =  ["0xe678bf9f07F46511045E7730Ebd1543D4b868c08"]
- const chainlinkAddress = "0xa54B7d7bb36950c1e9cc49acd68Fb35247880cbf"
+ const chainlinkAddress = "0x0bF4e7bf3e1f6D6Dc29AA516A33134985cC3A5aA"
  module.exports = {
      exchangeAddresses,
      chainlinkAddress
