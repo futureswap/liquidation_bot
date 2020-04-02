9c7bd517c7b8c1d933072ec3ce4abb686c35bb68
JesseAbram <jesseabramowitz@hotmail.com>
2020-04-02 12:58:42 -0400

    dynamically get chianlink address

--- blockchainServices/Addresses.js
+++ blockchainServices/Addresses.js
@@ -1,6 +1,4 @@
  const exchangeAddresses =  ["0xCeC3b509093B341399B9083678a2bD96429A0A6C"]
- const chainlinkAddress = "0xF75D82b2A3981fC1003402d1B06CB94dd6cb0540"
  module.exports = {
      exchangeAddresses,
-     chainlinkAddress
  }
--- blockchainServices/getOracleUpdates.js
+++ blockchainServices/getOracleUpdates.js
@@ -1,5 +1,5 @@
 const {chainlinkAbi} =  require("./chainlinkInstance")
-const {chainlinkAddress} =  require("./Addresses")
+const {chainlinkAddress} =  require("./getChainlinkAddress")
 const ethers = require('ethers');
 const {liquidationCheck} = require('./liqudate')
 const {URL, NETWORK} = require("../src/config/configurations")
@@ -8,7 +8,8 @@ const {URL, NETWORK} = require("../src/config/configurations")
 const provider = URL ? new ethers.providers.JsonRpcProvider(URL) : new ethers.getDefaultProvider(NETWORK)
 
 const listenForChainlinkUpdate = async () => {
-    const contract = new ethers.Contract(chainlinkAddress, chainlinkAbi, provider);
+    const address = await chainlinkAddress()
+    const contract = new ethers.Contract(address, chainlinkAbi, provider);
     contract.on("AnswerUpdated", async (current) => {
         liquidationCheck((Number(current) * 10000000000))
     })
